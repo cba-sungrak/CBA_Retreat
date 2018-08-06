961cad968ec6bcdef49476983b1cfea97b97f278
truelightn <truelightn@gmail.com>
2018-08-07 00:38:17 +0900

    gradle 오류 수정

--- app/src/main/java/kr/or/sungrak/cba/cba_retreat/fragment/PostListFragment.java
+++ app/src/main/java/kr/or/sungrak/cba/cba_retreat/fragment/PostListFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DataSnapshot;
