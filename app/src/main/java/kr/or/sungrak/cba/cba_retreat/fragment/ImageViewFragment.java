11b5ae1e733d2f8b865869c8a188cf5f876adda7
jinhwan.na <jinhwan.na@lge.com>
2018-12-14 17:33:01 +0900

    이미지 업데이트 시간을 저장해서, 현재 이미지 업데이트 시간이 변경되었으면 이미지도 변경되도록 변경 쉐어드프레퍼런스추가

--- app/src/main/java/kr/or/sungrak/cba/cba_retreat/fragment/ImageViewFragment.java
+++ app/src/main/java/kr/or/sungrak/cba/cba_retreat/fragment/ImageViewFragment.java
@@ -1,8 +1,11 @@
 package kr.or.sungrak.cba.cba_retreat.fragment;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,44 +23,55 @@ import uk.co.senab.photoview.PhotoView;
 @SuppressLint("ValidFragment")
 public class ImageViewFragment extends Fragment {
     String mImage;
-    String mUpdateTime="";
+
     StorageReference pathReference;
     PhotoView imageView;
+    SharedPreferences sharedPref;
+
     @SuppressLint("ValidFragment")
     public ImageViewFragment(String image) {
         mImage = image;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View rootView = inflater.inflate(R.layout.ui_layout, container, false);
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
         StorageReference storageReference = FirebaseStorage.getInstance().getReference();
         // Create a reference with an initial file path and name
-         pathReference = storageReference.child(mImage);
+        pathReference = storageReference.child(mImage);
 
         // ImageView in your Activity
-         imageView = rootView.findViewById(R.id.imageView2);
+        imageView = rootView.findViewById(R.id.imageView2);
+
 
         pathReference.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
             @Override
             public void onSuccess(StorageMetadata storageMetadata) {
-                mUpdateTime = String.valueOf(storageMetadata.getCreationTimeMillis());
-                GlideApp.with(getActivity())
-                        .load(pathReference)
-                        .signature(new ObjectKey(mUpdateTime))
-                        .into(imageView);
+                String previousUpdateTime = sharedPref.getString(mImage, "");
+                String updatetime = String.valueOf(storageMetadata.getCreationTimeMillis());
+                if (!previousUpdateTime.equalsIgnoreCase(updatetime)) {
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString(mImage, updatetime);
+                    editor.commit();
+                    Log.d("CBA_ImageViewFragment", "image/" + mImage + " UpdateTime/" + updatetime);
+                    GlideApp.with(getActivity())
+                            .load(pathReference)
+                            .signature(new ObjectKey(updatetime))
+                            .into(imageView);
+                }
             }
         });
-
 // Load the image using Glide
 //       // ImageView in your Activity
-//        GlideApp.with(getActivity())
-//                .load(pathReference)
-//                .signature(new ObjectKey(UUID.randomUUID().toString()))
-//                .into(imageView);
+        String previousUpdateTime = sharedPref.getString(mImage, "");
+        GlideApp.with(getActivity())
+                .load(pathReference)
+                .signature(new ObjectKey(previousUpdateTime))
+                .into(imageView);
         return rootView;
     }
     // Reference to an image file in Firebase Storage
